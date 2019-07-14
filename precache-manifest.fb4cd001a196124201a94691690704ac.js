7b0a46810de7673ff38cedc635c2c3d570923138
mobilecom21 <mobile.com21@yahoo.com>
2019-07-14 18:34:23 +0300

    website uploaded

---
+++ precache-manifest.fb4cd001a196124201a94691690704ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942a1172a5bdee5d0d59e7f353be2967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57376090aa3f367078ec",
+    "url": "/static/css/2.32bbf376.chunk.css"
+  },
+  {
+    "revision": "4770791b8fcff054b453",
+    "url": "/static/css/main.5b5b800a.chunk.css"
+  },
+  {
+    "revision": "57376090aa3f367078ec",
+    "url": "/static/js/2.85f0ef7b.chunk.js"
+  },
+  {
+    "revision": "4770791b8fcff054b453",
+    "url": "/static/js/main.7eee10fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac51ad8a36d5bab6da11d454bcd06561",
+    "url": "/static/media/ProximaNova-Light_0.ac51ad8a.otf"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/static/media/ProximaNova-Regular_0.bf9f5d50.otf"
+  },
+  {
+    "revision": "f14eee643541cf03a10f26c944cc29f5",
+    "url": "/static/media/ProximaNova-Semibold_0.f14eee64.otf"
+  }
+]);
